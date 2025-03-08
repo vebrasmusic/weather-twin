aba0ba0332aa2c71b5d73b7a79dc48dd281972a9
Andrés Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2025-03-08 14:37:09 -0800

    error cause idk why it happened

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {APIProvider, Map} from '@vis.gl/react-google-maps';
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Toaster richColors />
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-'use client'
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 
-import { MainForm } from "@/components/andres/mainForm"
+import { MainForm } from "@/components/andres/mainForm";
 import { Separator } from "@/components/ui/separator";
 import axios from "axios";
 import { useState } from "react";
@@ -9,55 +9,70 @@ import { APIProvider, Map } from "@vis.gl/react-google-maps";
 import { MainDiv } from "@/components/andres/mainDiv";
 import { LoadingDiv } from "@/components/andres/loadingDiv";
 import { ResultsDiv } from "@/components/andres/resultsDiv";
+import { toast } from "sonner";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL
-
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export default function Home() {
-
   const [globalMatchedCities, setGlobalMatchedCities] = useState<any>([]);
   const [queryCity, setQueryCity] = useState<any>([]);
   const [USMatchedCity, setUSMatchedCity] = useState<any>([]);
   const [currentState, setCurrentState] = useState("main");
 
+  useEffect(() => {
+    toast.error(
+      "Heads up, just discovered some error in the API I use for coordinates. Don't have time to switch it out - a. 2025"
+    );
+  }, []);
+
   const renderDiv = () => {
-    switch (currentState){
+    switch (currentState) {
       case "loading":
-        return <LoadingDiv/>
+        return <LoadingDiv />;
       case "main":
-        return <MainDiv onSubmit={onSubmit}/>
+        return <MainDiv onSubmit={onSubmit} />;
       case "success":
-        return <ResultsDiv globalMatchedCities={globalMatchedCities} queryCity={queryCity} USMatchedCity={USMatchedCity}/>
-      case "error":
-        return <div>Error</div>
+        return (
+          <ResultsDiv
+            globalMatchedCities={globalMatchedCities}
+            queryCity={queryCity}
+            USMatchedCity={USMatchedCity}
+          />
+        );
+      case "error": {
+        toast.error(
+          "We're getting some errors, something is broken in the city > coordinate matching API I use sorry - andrés, 03/25"
+        );
+        return <div>Error</div>;
+      }
       default:
-        return <MainDiv onSubmit={onSubmit}/>
+        return <MainDiv onSubmit={onSubmit} />;
     }
-  }
+  };
 
   async function onSubmit(values: any) {
     const params = {
-        cityName: values.cityName
-    }
+      cityName: values.cityName,
+    };
 
     try {
-        setCurrentState("loading");
-        const response = await axios.get(`${apiUrl}/cities/match`, {params});   
-        //TODO: loading screen
-        const globalMatchedCityObject = response.data.globalMatchedCities;
-        const queryCityObject = response.data.queryCity;
-        const USMatchedCityObject = response.data.USMatchedCity;
-        //console.log(matchedCities);
-        setGlobalMatchedCities(globalMatchedCityObject);
-        setQueryCity(queryCityObject);
-        setUSMatchedCity(USMatchedCityObject);
-        setCurrentState("success");
+      setCurrentState("loading");
+      const response = await axios.get(`${apiUrl}/cities/match`, { params });
+      //TODO: loading screen
+      const globalMatchedCityObject = response.data.globalMatchedCities;
+      const queryCityObject = response.data.queryCity;
+      const USMatchedCityObject = response.data.USMatchedCity;
+      //console.log(matchedCities);
+      setGlobalMatchedCities(globalMatchedCityObject);
+      setQueryCity(queryCityObject);
+      setUSMatchedCity(USMatchedCityObject);
+      setCurrentState("success");
     } catch (error) {
-        setCurrentState("error");
-        console.log(error);
-        //TODO: error message did u type it in right?
+      setCurrentState("error");
+      console.log(error);
+      //TODO: error message did u type it in right?
     }
-}
+  }
 
   return (
     <main>
@@ -67,7 +82,15 @@ export default function Home() {
         {renderDiv()}
         {/* footer */}
         <div className="absolute bottom-2">
-          <h3 className="text-slate-400 font-bold text-sm">Made by <a href="https://andresduvvuri.com" className="text-slate-300 hover:text-slate-400">Andrés Duvvuri</a></h3>
+          <h3 className="text-slate-400 font-bold text-sm">
+            Made by{" "}
+            <a
+              href="https://andresduvvuri.com"
+              className="text-slate-300 hover:text-slate-400"
+            >
+              Andrés Duvvuri
+            </a>
+          </h3>
         </div>
       </section>
       {/* about section
