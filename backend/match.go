b8d7afb91ddfcecb305acc28bb78687459f76723
Andrés Duvvuri <128623334+vebrasmusic@users.noreply.github.com>
2025-03-12 19:38:23 -0700

    restart

--- backend/match.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "net/http"
-
-// create a struct we attach some methods to as the route handlers
-type MatchHandler struct{}
-
-func (m MatchHandler) GetMatches(w http.ResponseWriter, r *http.Request) {
-}
