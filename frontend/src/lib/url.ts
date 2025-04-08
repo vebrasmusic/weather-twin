341452edc68ce7acee28a47bd1cd4d1ed1678657
Andres <vebrasmusic@gmail.com>
2025-04-08 04:48:09 +0000

    fixe dsdome stuf, rate limit backedn

--- frontend/src/lib/url.ts
+++ frontend/src/lib/url.ts
@@ -1,8 +1,9 @@
-import { env } from "process";
 
-const stage = env.STAGE || "dev";
+const stage = process.env.NEXT_PUBLIC_STAGE;
+
+console.log(stage)
 export const baseUrl =
-  stage === "dev" ? "http://localhost:8000/api" : "/weather-twin/api";
+  stage === "dev" ? "http://localhost:8000/api" : "https://wt.andresduvvuri.com/api";
 
 export const socketUrl =
-  stage === "dev" ? "ws://localhost:8000/api/ws" : "/weather-twin/api/ws";
+  stage === "dev" ? "ws://localhost:8000/api/ws" : "wss://wt.andresduvvuri.com/api/ws";
